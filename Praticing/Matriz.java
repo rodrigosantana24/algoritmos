937551f7c5679a864a6a77a0ab4e1aafb4972e79
Rodrigo Santana <rodrigosantana.dev@gmail.com>
2026-03-29 19:24:32 -0300

    Implementação de uma Matriz em Java

---
+++ Praticing/Matriz.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Matriz{
+    public static void main(String[] args){     
+        Scanner sc = new Scanner(System.in);
+        
+        final int linhas = 3;
+        final int colunas = 5;
+        int[][] matriz = new int[linhas][colunas];
+
+        for(int i=0; i<linhas; i++){
+            for(int j=0; j<colunas; j++){
+                matriz[linhas][colunas] = sc.nextInt();
+            }
+        }
+
+        for(int i=0; i<linhas; i++){
+            for(int j=0; j<colunas; j++){
+                System.out.println(matriz[linhas][colunas]);
+            }
+        }
+        sc.close();
+    }
+}
